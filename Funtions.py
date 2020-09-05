1760ce88ed4bddcf29e8987a6b7bf79a72c2f286
Jesussilverioe <58699576+Jesussilverioe@users.noreply.github.com>
2020-09-04 19:43:32 -0500

    Update the functions file

---
+++ Funtions.py
@@ -0,0 +1,15 @@
+# Pratice creating functions
+# Functions make a program easier to write, read, and fix code.
+
+# Function to print a string
+def somefunc():
+    print ("This is a Function!!!!")
+
+somefunc()
+
+def ReturnFunc():
+    return "This is return by a function."
+
+stringVal = ReturnFunc()
+
+print (stringVal)
--- Loops.py
+++ Loops.py
@@ -1,6 +1,22 @@
 num = [1,2,3,4]
 
+# Regular loop without range
+# Range is define as FROM - TO interval. The loop will iterate withing that interval 
+
 for x in num:
-    if x == 2:
-        break
     print(x)
+    for y in 'abc':
+        print(x, y)
+
+# Using range in a for loop
+
+for i in range (1, 11) :
+    print (i)
+
+
+# While loop. In a while loop you define the condition to stop the loop.
+# One has to be very careful no the enter in a infinate loop
+t = 0
+while t <= 10:
+    print(t)
+    t += 1
